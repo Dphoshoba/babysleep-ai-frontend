905dae2ca3eba932e863a71b1852653273ab5169
David George <davidoshoba@gmail.com>
2025-07-07 11:37:08 +0930

    Create placeholder pages for Parent Information and Referral Rewards

---
+++ src/pages/ReferralRewards.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReferralRewards = () => {
+  return (
+    <div>
+      <h1>Referral Rewards</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default ReferralRewards;
